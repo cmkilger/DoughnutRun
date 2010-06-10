da72eb72698a8b3f12645c70469f4e98a0dbad69
Cory Kilger <cmkilger@gmail.com>
2010-06-10 16:31:33 -0700

    Added Xcode project

---
+++ DoughnutRun/Classes/DoughnutRunAppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  DoughnutRunAppDelegate.h
+//  DoughnutRun
+//
+//  Created by Cory Kilger on 6/10/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface DoughnutRunAppDelegate : NSObject <UIApplicationDelegate> {
+
+	UIWindow *window;
+
+	NSManagedObjectContext *managedObjectContext;
+	NSManagedObjectModel *managedObjectModel;
+	NSPersistentStoreCoordinator *persistentStoreCoordinator;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ DoughnutRun/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  DoughnutRun
+//
+//  Created by Cory Kilger on 6/10/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+
+}
+
