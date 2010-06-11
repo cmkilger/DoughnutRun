149874377be00ecad99608cf2fef77e341a0e83e
Sean Clark Hess <seanhess@gmail.com>
2010-06-10 17:57:41 -0700

    started on receipts view

---
+++ DoughnutRun/Classes/ReceiptSummaryViewController.h
@@ -0,0 +1,16 @@
+//
+//  ReceiptSummaryViewController.h
+//  DoughnutRun
+//
+//  Created by Sean Hess on 6/10/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ReceiptSummaryViewController : UIViewController {
+    
+}
+
+@end
