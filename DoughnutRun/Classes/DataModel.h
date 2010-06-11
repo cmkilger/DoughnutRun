6d19e79d00c6de8588482eb743d19fedb5ebc9b6
Cory Kilger <cmkilger@gmail.com>
2010-06-10 20:53:36 -0700

    You guys need to fix your company names in your preferences.

--- DoughnutRun/Classes/DataModel.h
+++ DoughnutRun/Classes/DataModel.h
@@ -3,7 +3,7 @@
 // DoughnutRun
 //
 // Created by Sean Hess on 6/10/10.
-// Copyright 2010 __MyCompanyName__. All rights reserved.
+// Copyright 2010 Cory Kilger. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
